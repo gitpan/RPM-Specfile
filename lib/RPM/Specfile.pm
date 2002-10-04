c222bb8805af2f6204ffd088202a6fc0ba895de6
Chip Turner <cturner@pattern.net>
2002-10-04 00:38:25 +0000

    Import of CHIPT/RPM-Specfile-1.09 from CPAN.
    gitpan-cpan-distribution: RPM-Specfile
    gitpan-cpan-version:      1.09
    gitpan-cpan-path:         CHIPT/RPM-Specfile-1.09.tar.gz
    gitpan-cpan-author:       CHIPT
    gitpan-cpan-maturity:     released

--- lib/RPM/Specfile.pm
+++ lib/RPM/Specfile.pm
@@ -6,7 +6,7 @@ use strict;
 
 use vars qw/$VERSION/;
 
-$VERSION = '1.08';
+$VERSION = '1.09';
 
 sub new {
   my $class = shift;
@@ -88,6 +88,8 @@ sub add_changelog_entry {
   my $self = shift;
   my $who = shift;
   my $entry = shift;
+
+  POSIX::setlocale( &POSIX::LC_ALL, "C" );
 
   my $output;
   $output .= strftime("* %a %b %d %Y $who\n", localtime time);
