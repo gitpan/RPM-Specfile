2108253b8ea723100aabf4ab89f886139e867a0b
Chip Turner <cturner@pattern.net>
2004-03-29 15:53:45 +0000

    Import of CHIPT/RPM-Specfile-1.16 from CPAN.
    gitpan-cpan-distribution: RPM-Specfile
    gitpan-cpan-version:      1.16
    gitpan-cpan-path:         CHIPT/RPM-Specfile-1.16.tar.gz
    gitpan-cpan-author:       CHIPT
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -11,6 +11,7 @@ use RPM::Specfile;
 ok(1); # If we made it this far, we're ok.
 
 my $spec = new RPM::Specfile;
+$spec->macros('%define a_macro     by any other name is still a_macro.');
 $spec->name('blippy-foo');
 $spec->version('1.00');
 $spec->release('1');
@@ -24,6 +25,7 @@ $spec->push_require("foo >= 1.2");
 $spec->push_require("foo2 >= 1.2");
 $spec->push_buildrequire("xfoo >= 1.2");
 $spec->push_buildrequire("xfoo2 >= 1.2");
+$spec->push_prefix('/usr/local/mypkg');
 $spec->description(<<E);
 This is an extra blippy module.  It slices, it dices.
 Author: Blippy Foo <cturner\@redhat.com>
